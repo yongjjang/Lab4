cab8a67f0383d20190373e380ecf756b93d07193
yongjjang <pastelhawk@gmail.com>
2019-12-17 13:54:35 +0900

    9번 데이터베이스 사용 프로그램 구현 완료

--- no9/no9.c
+++ no9/no9.c
@@ -6,12 +6,24 @@
 #include <stdlib.h>
 #include <mysql.h>
 
+#define CREATE 2
+#define READ 1
+#define UPDATE 3
+#define DELETE 4
+#define QUIT 5
 
 int id = 0;
 char name[20];
 char phone_num[20];
 char address[20];
 char buffer[100];
+
+MYSQL mysql;
+MYSQL_RES* res;
+
+int i, fields, user_select = 0;
+
+MYSQL_ROW row;
 
 void input_data(){
     printf("ID : ");
@@ -24,62 +36,87 @@ void input_data(){
     scanf("%s", address);
 }
 
-//void read(){
-//    if (mysql_query(&mysql, "select * from people")) {
-//        printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
-//        exit(1);
-//    }
-//    printf("조회가 완료되었습니다.\n");
-//}
-//
-//void create(){
-//    input_data();
-//    sprintf(buffer, "insert into people values(%d, \"%s\",\"%s\",\"%s\");", id, name, phone_num, address);
-//
-//    if (mysql_query(&mysql, buffer)) {
-//        printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
-//        exit(1);
-//    }
-//    printf("생성가 완료되었습니다.\n");
-//}
-//
-//void delete(){
-//    printf("INPUT ID\n");
-//    printf("ID : ");
-//    scanf("%d", &id);
-//    sprintf(buffer, "delete from person where id = %d", id);
-//    if (mysql_query(&mysql, buffer)) {
-//        printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
-//        exit(1);
-//    }
-//    printf("삭제가 완료되었습니다.\n");
-//}
 
-void main() {
-    MYSQL mysql;
-
+void run(){
     mysql_init(&mysql);
-    memset(buffer, 0, sizeof(char));
-
 
     if (!mysql_real_connect(&mysql, "localhost", "root", "q", NULL, 0, (char *) NULL, 0)) {
         printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
         exit(1);
     }
 
-    if (mysql_query(&mysql, "use people;")) {
+    if (mysql_query(&mysql, "use people")) {
         printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
         exit(1);
     }
 
-    if (mysql_query(&mysql, "select * from people;")) {
-        printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
-        exit(1);
-    }else
-        printf("조회가 완료되었습니다.\n");
+    printf("데이터베이스 프로그램입니다.");
+    while (user_select != QUIT) {
+        printf("\n원하시는 기능을 선택해 주세요!");
+        printf("\n\t1. 조회\n\t2. 삽입\n\t3. 수정\n\t4. 삭제\n\t5. 종료\n");
+        printf("Input > ");
 
-    printf("query info: %s\n", mysql_info(&mysql));
-    printf("affected rows: %lu\n", (unsigned long) mysql_affected_rows(&mysql));
+        scanf("%d", &user_select);
 
-    mysql_close(&mysql);
+        switch (user_select){
+            case READ:
+                if (mysql_query(&mysql, "select * from people")) {
+                    printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
+                    exit(1);
+                } else
+                    printf("조회가 완료되었습니다.\n");
+                res = mysql_use_result(&mysql);
+                fields = mysql_num_fields(res);
+                printf("Total student is as follows.\n");
+                while ((row = mysql_fetch_row(res))) {
+                    for (i = 0; i < fields; ++i) {
+                        printf("%12s ", row[i]);
+                    }
+                    printf("\n");
+                }
+                mysql_free_result(res);
+                break;
+
+            case CREATE:
+                input_data();
+                sprintf(buffer, "insert into people values(%d, \"%s\",\"%s\",\"%s\");", id, name, phone_num, address);
+
+                if (mysql_query(&mysql, buffer)) {
+                    printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
+                    exit(1);
+                }
+                printf("생성이 완료되었습니다.\n");
+                break;
+
+            case DELETE:
+                printf("삭제할 ID를 입력해 주세요!\n");
+                printf("ID : ");
+                scanf("%d", &id);
+                sprintf(buffer, "delete from people where id = %d", id);
+                if (mysql_query(&mysql, buffer)) {
+                    printf("Error %d: %s\n", mysql_errno(&mysql), mysql_error(&mysql));
+                    exit(1);
+                }
+                printf("삭제가 완료되었습니다.\n");
+                break;
+
+            case UPDATE:
+                input_data();
+                sprintf(buffer, "update people set name=\'%s\',phonenum=\'%s\',address=\'%s\' where id=%d", name, phone_num, address, id);
+                if (mysql_query(&mysql, buffer)) {
+                    printf("Error %d: %s\n", mysql_errno(&mysql),  mysql_error(&mysql));
+                    exit(1);
+                }
+                break;
+
+            case QUIT:
+                printf("\n 이용해주셔서 감사합니다. 안녕히 가세요!");
+                mysql_close(&mysql);
+                exit(1);
+        }
+    }
+}
+
+void main() {
+    run();
 }
